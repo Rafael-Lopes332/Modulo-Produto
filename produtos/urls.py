c38f6782405dbed6b38b239b6d57ef4881ba85e8
Rafael-Lopes332 <rafalsilva.ti@gmail.com>
2026-06-22 21:34:05 -0300

    atualizações finais

--- produtos/urls.py
+++ produtos/urls.py
@@ -5,11 +5,10 @@ from rest_framework.routers import DefaultRouter
 from .views import ProdutoViewSet
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'produtos', ProdutoViewSet)
+router.register(r'produtos', ProdutoViewSet, basename='produto')
 
 urlpatterns = [
-    path("api/produtos/", ProdutoViewSet.as_view()),
-    path("api/produtos/listarprodutos", ProdutoViewSet.as_view()),
+    path('api/produtos', ProdutoViewSet.as_view(), name='api-listar-produtos'),
     path('listarprodutos', views.listarProdutos),
     path('cadastroProduto', views.cadastroProduto),
     path('excluirProduto/<int:id>', views.excluirProduto),
