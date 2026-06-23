ea4ab6b6b8d6caa083fadef9e805f103f27ee236
Rafael-Lopes332 <rafalsilva.ti@gmail.com>
2026-06-22 21:54:54 -0300

    atualizações finais

--- sistema/urls.py
+++ sistema/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1', include('produtos.urls')),
+    path('api/v1/', include('produtos.urls')),
     path('admin/', admin.site.urls),
     path('produtos/', include('produtos.urls'))
 ]
