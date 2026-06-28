fbb1650c61cbd2bff1ffbf47ef614de3ecdd1838
Rafael-Lopes332 <rafalsilva.ti@gmail.com>
2026-06-28 10:25:06 -0300

    Atualização das rotas e inclusão de saída da sessão

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from . import views
+from .import views
 
 from rest_framework.routers import DefaultRouter
 from .views import ProdutoViewSet
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'produtos', ProdutoViewSet, basename='produto')
+router.register(r'api/produtos', ProdutoViewSet, basename='produto')
 
 urlpatterns = [
-    path('produtos', ProdutoViewSet.as_view({'get': 'list'}), name='api-listar-produtos'),
+    path('api/produtos', ProdutoViewSet.as_view({'get': 'list'}), name='api-listar-produtos'),
+    path('api/produtos', ProdutoViewSet.as_view({'post': 'create'}), name='api-criar-produto'),
+    path('api/produtos/<int:pk>', ProdutoViewSet.as_view({'get': 'retrieve'}), name='api-detalhar-produto'),
+    path('api/produtos/<int:pk>', ProdutoViewSet.as_view({'put': 'update'}), name='api-editar-produto'),
+    path('api/produtos/<int:pk>', ProdutoViewSet.as_view({'patch': 'partial_update'}), name='api-atualizar-parcial-produto'),
+    path('api/produtos/<int:pk>', ProdutoViewSet.as_view({'delete': 'destroy'}), name='api-excluir-produto'),
+
     path('listarprodutos', views.listarProdutos),
     path('cadastroProduto', views.cadastroProduto),
     path('excluirProduto/<int:id>', views.excluirProduto),
